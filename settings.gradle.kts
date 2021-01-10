8612728489eff901b7e7fd1ce3192cef0f7d8131
Lukas Jost <lukas.jost@outlook.com>
2021-01-10 14:51:18 +0100

    added gradle enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,26 @@
 rootProject.name = "NBTStorage"
+
+plugins {
+    id("com.gradle.enterprise").version("3.5.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = false
+    }
+    remote(HttpBuildCache::class) {
+        url = uri("https://repository.playlegend.net/artifactory/cache/")
+        credentials {
+            username = System.getenv("repositoryUser")
+            password = System.getenv("repositoryPassword")
+        }
+        isPush = true;
+    }
+}
